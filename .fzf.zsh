2b25ee9ead61dc6d70f4f8e852b8a56cb0ecab68
Andreas Kappler <andreas.kappler@adesso-afida.com>
2022-10-05 06:54:43 +0200

    zsh

---
+++ .fzf.zsh
@@ -0,0 +1,16 @@
+export FZF_CTRL_R_OPTS="--min-height=5 --height=10"
+export FZF_DEFAULT_COMMAND='ag -p ~/.gitignore -g ""'
+
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
